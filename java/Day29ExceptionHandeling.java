97d019bb6b52b162032da30fbafc120e2b1afe45
Sonu0126 <imsonikasubedi@gmail.com>
2023-07-26 22:04:08 -0500

    added

---
+++ java/Day29ExceptionHandeling.java
@@ -0,0 +1,72 @@
+
+public class Day29ExceptionHandeling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Program 1 without exception
+//		System.out.println("Sonu");
+//		System.out.println(7/0);
+//		System.out.println("read");
+
+		// Program 2 (with exception)
+//		System.out.println("Sonu");
+//		try {
+//			int dividbyzero = 7/0;
+//			System.out.println("Rest of the code on try block");
+//		}
+//		catch(ArithmeticException e){
+//			System.out.println(e.getMessage());
+//		}
+//		System.out.println("read");
+//		
+
+		// Program 3
+//		System.out.println("Define an array with values");
+//		int[] num = { 5, 10, 15, 20 };
+//		try {
+//			System.out.println(num[0]);
+//			System.out.println(num[5]);
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//
+//		}
+//		System.out.println("Moving ahead after managing the exception");
+//		
+		// Program 4
+//		System.out.println("Define an array with values");
+//		int[] number = {2,4,6,8};
+//		try {
+//			System.out.println(number[2]);
+//			System.out.println(number[6]);
+//			System.out.println(5/0);
+//		}
+//		catch(ArrayIndexOutOfBoundsException e) {
+//			System.out.println("please enter valid index of array");
+//			System.out.println(e.getMessage());
+//		}
+//		catch(ArithmeticException e) {
+//			System.out.println("Please enter valid input");
+//			System.out.println(e.getMessage());
+//		}
+//		catch(Exception e) {
+//			System.out.println("Exception handled successfully.");
+//			
+//		}
+//		System.out.println("Moving ahead after managing the exception");
+
+		// Program 5
+//		System.out.println("Namaste");
+//		try {
+//			System.out.println(2 / 2);
+//
+//		} catch (Exception e) {
+//			System.out.println("Exception managed successully");
+//			String message = e.getMessage();
+//			System.out.println(message);
+//		} finally {
+//			System.out.println("Message will be executed anyhow");
+//		}
+
+	}
+
+}
